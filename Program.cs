fd7fc36e6a5eb65357fc3a732859ad4a88b894e6
Aloidin <aloidin.akram@gmail.com>
2025-06-13 09:02:07 +0300

    Task1

--- Program.cs
+++ Program.cs
@@ -1,3 +1,50 @@
-﻿using Homeworks.NewFolder;
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
 
-FileName
+        Book book1 = new Book("Atomic Habits", "James Clear", 2018);
+
+
+        Console.WriteLine(book1.GetInfo());
+
+
+        if (book1.IsPublishedRecently())
+        {
+            Console.WriteLine("This book was published recently.");
+        }
+        else
+        {
+            Console.WriteLine("This book is not published recently.");
+        }
+    }
+}
+
+
+public class Book
+{
+    public string Title { get; set; }
+    public string Author   { get; set; }
+    public int Year  { get; set; }
+
+    public Book (string  title, string author, int year)
+    {
+        Title = title;
+        Author = author;
+        Year =  year;
+    }
+
+    public string  GetInfo()
+    {
+        return   $"Title: {Title}, Author: {Author}, Year: {Year}";
+    }
+
+
+    public bool  IsPublishedRecently()
+    {
+        return  Year  >  2010;
+    }
+}
+
+
